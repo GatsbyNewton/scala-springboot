c9c7066a6a388d9941a94f3adf5b3538b0a22295
GatsbyNewton <wangzmking@163.com>
2017-10-23 09:17:37 +0800

    Spring boot bootstrap

---
+++ src/main/scala/edu/wzm/App.scala
@@ -0,0 +1,18 @@
+package edu.wzm
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.{EnableAutoConfiguration, SpringBootApplication}
+import org.springframework.context.annotation.ComponentScan
+
+/**
+ * Hello world!
+ *
+ */
+@ComponentScan
+//@EnableAutoConfiguration
+@SpringBootApplication
+class Config
+
+object App extends App {
+    SpringApplication.run(classOf[Config])
+}
